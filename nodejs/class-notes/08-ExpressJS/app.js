cea25bf10f66da962611f0e9c8093264c9c443e7
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-28 10:55:03 +0200

    env00012

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -1,16 +1,16 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS
 ------------------------------------------------------- */
 /*
  * npm init -y
  * npm i express dotenv
-*/
+ */
 
 /* ExpressJS Start */
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-
-
-
+/* ENV */
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000
