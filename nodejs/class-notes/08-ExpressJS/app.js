9b1324798fc11f1414c858206f2d6a85b9d0ccb5
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-28 10:40:47 +0200

    exp001

---
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -0,0 +1,16 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS
+------------------------------------------------------- */
+/*
+ * npm init -y
+ * npm i express dotenv
+*/
+
+/* ExpressJS Start */
+const express = require('express')
+const app = express()
+
+
+
+
