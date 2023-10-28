b56342c6701faf1bda2912c853c2c4ae608df7cf
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-28 11:02:59 +0200

    env00015

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -15,3 +15,9 @@ const app = express();
 require("dotenv").config();
 const HOST = process.env.HOST || "http://localhost";
 const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Running on ${HOST}:${PORT}`))
+
+
+/*---------------------------------------------------------------*/
+
