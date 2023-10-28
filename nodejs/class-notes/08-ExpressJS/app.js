2cb397b821295644a15b55ca3963920e10d70c4e
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-28 12:45:14 +0200

    exp00017

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -13,16 +13,16 @@ const app = express();
 
 /* ENV */
 require("dotenv").config();
-const HOST = process.env.HOST || "http://localhost";
+const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 8000;
 /*---------------------------------------------------------------*/
 /* HTTP_Methods & URLs */
 
-app.get('/', (request, response) => {
-
-})
-
-
+app.get("/", (request, response) => {
+  //     //? run response.send for print-out:
+  response.send("Welcome to Express");
+});
 
 /*---------------------------------------------------------------*/
-app.listen(PORT, () => console.log(`Running on ${HOST}:${PORT}`));
+// app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
