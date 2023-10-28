26cf6cb0efc8c0829cfd2e07f31ff66ce0859dc6
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-28 13:07:47 +0200

    env00018

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -1,28 +1,34 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESSJS
 ------------------------------------------------------- */
 /*
  * npm init -y
  * npm i express dotenv
- */
+*/
 
 /* ExpressJS Start */
-const express = require("express");
-const app = express();
+const express = require('express'); // Assing expressFramework to express variable.
+const app = express() // run application on express.
 
 /* ENV */
-require("dotenv").config();
-const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || 8000;
+require('dotenv').config()
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 8000
 /*---------------------------------------------------------------*/
 /* HTTP_Methods & URLs */
 
-app.get("/", (request, response) => {
-  //     //? run response.send for print-out:
-  response.send("Welcome to Express");
+app.get('/', (request, response) => {
+  //? run response.send for print-out:
+  // response.send( 'Welcome to Express' )
+  response.send({ message: 'called in "get" method '})
 });
+app.post('/', (request, response) => response.send({ message: "called in 'post' method."}))
+app.put('/', (request, response) => response.send({ message: "called in 'put' method."}))
+app.delete('/', (request, response) => response.send({ message: "called in 'delete' method."}))
+
+
 
 /*---------------------------------------------------------------*/
 // app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
-app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`))
