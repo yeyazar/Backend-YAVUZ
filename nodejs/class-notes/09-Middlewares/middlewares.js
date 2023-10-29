18328ab96f5073805ffbcd8c40dabec75ea414c1
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-29 07:53:28 +0100

    exp09003

--- nodejs/class-notes/09-Middlewares/middlewares.js
+++ nodejs/class-notes/09-Middlewares/middlewares.js
@@ -13,9 +13,33 @@ const PORT = process.env.PORT || 8000;
 //? Middleware functions must be has three parameters.
 //? Last parameter is for next().
 
+//* next() for next Function:
+
+// Middleware:
+app.get('/', (req, res, next) => {
+
+    // Request/Response parametresi ile next() metoduna veri gönderebilirim.
+    // req.customData = 'Custom Data With Request'
+    // res.customDataWithResponse = 'Custom Data With Response'
+
+    next() // Go to next Function.
+
+     // next() komutundan sonra çıktı vermek anlamsız olacaktır. (Hata verecektir.)
+    res.send({
+        message: 'Middleware running'
+    })
+
+})
+
 app.get("/", (req, res) => {
+
+
   res.send({
-    message: "Welcome to Home",
+    customData: [
+        req.customData,
+        res.customDataWithResponse
+    ],
+    message: "Welcome",
   });
 });
 
