d4025e11645aa076ecb715749998996e81b4f01d
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-29 07:32:16 +0100

    exp09001

---
+++ nodejs/class-notes/09-Middlewares/middlewares.js
@@ -0,0 +1,25 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - MIDDLEWARES
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+//? Middleware functions must be has three parameters.
+//? Last parameter is for next().
+
+
+
+
+app.get('/', (req, res) => {
+    
+})
+
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
