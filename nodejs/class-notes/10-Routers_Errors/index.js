1962edd487cafa41160205f72e6c9e4497a57457
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-30 15:22:05 +0100

    exp10

--- nodejs/class-notes/10-Routers_Errors/index.js
+++ nodejs/class-notes/10-Routers_Errors/index.js
@@ -9,31 +9,22 @@ const PORT = process.env.PORT || 8000;
 /* ------------------------------------------------------- */
 //? "Router" is special app for URL control in ExpressJS.
 
-// app.get("/", (req, res) => {
-// 	res.send({ message: "Home Page" })
-// })
-// app.get("/about", (req, res) => {
-// 	res.send({ message: "About Page" })
-// })
-// app.get("/user:userId", (req, res) => {
-// 	res.send({ message: "User Page" })
-// })
-
+// app.get("/", (req, res) => {res.send({ message: "Home Page" })})
+// app.get("/about", (req, res) => {res.send({ message: "About Page" })})
+// app.get("/user:userId", (req, res) => {res.send({ message: "User Page" })})
 
 // Send routing to Router:
-const router = express.Router();
-router.get("/", (req, res) => {
-	res.send({ message: "Home Page" });
-});
-router.get("/about", (req, res) => {
-	res.send({ message: "About Page" });
-});
-router.get("/user:userId", (req, res) => {
-	res.send({ message: "User Page" });
-});
+// const router = express.Router();
+// router.get("/", (req, res) => {res.send({ message: "Home Page" })});
+// router.get("/about", (req, res) => {res.send({ message: "About Page" })});
+// router.get("/user:userId", (req, res) => {res.send({ message: "User Page" })});
+// Router to App
+// app.use(router);
 
-//Router to App
-app.use(router);
+// const router = require('./routes/')
+// app.use(router)
+
+app.use(require("./routes/index.js"));
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
