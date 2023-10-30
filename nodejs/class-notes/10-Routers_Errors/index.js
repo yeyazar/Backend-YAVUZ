00144d9b00e9193b471e02d4b47260d9689557e5
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-30 15:19:34 +0100

    exp10

--- nodejs/class-notes/10-Routers_Errors/index.js
+++ nodejs/class-notes/10-Routers_Errors/index.js
@@ -19,6 +19,8 @@ const PORT = process.env.PORT || 8000;
 // 	res.send({ message: "User Page" })
 // })
 
+
+// Send routing to Router:
 const router = express.Router();
 router.get("/", (req, res) => {
 	res.send({ message: "Home Page" });
