0e8b751b6bb847ed46030fef5d6b4be0a61bea58
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-30 17:05:43 +0100

    exp10

--- nodejs/class-notes/10-Routers_Errors/errors.js
+++ nodejs/class-notes/10-Routers_Errors/errors.js
@@ -17,23 +17,66 @@ const PORT = process.env.PORT || 8000;
 /* ------------------------------------------------------- */
 
 app.get("/user/:id", (req, res) => {
-	try {
-		const id = req.params.id ?? 0;
-		if (isNaN(id)) {
-			throw new Error("ID is Not A Number", { cause: "params.id=" + id });
-		} else {
-			res.send({
-				error: false,
-				id: id,
-			});
-		}
-	} catch (err) {
+	const id = req.params.id ?? 0;
+	if (isNaN(id)) {
+		res.statusCode = 400;
+		throw new Error("ID is Not A Number", { cause: "params.id=" + id });
+	} else {
 		res.send({
-			error: true,
-			message: err.message,
+			error: false,
+			id: id,
 		});
 	}
 });
+
+/* ------------------------------------------------------- *
+// TRY-CATCH:
+
+app.get('/user/:id', (req, res, next) => {
+
+    try {
+        const id = req.params.id ?? 0
+        if (isNaN(id)) {
+            throw new Error('ID is Not A Number', { cause: 'params.id='+id })
+        } else {
+            res.send({ 
+                error: false, 
+                id: id
+            })
+        }
+    } catch (err) {
+
+        console.log('try-catch runned')
+        next(err) // Go to errorHandler()
+
+        res.send({ 
+            error: true, 
+            message: err.message,
+            cause: err.cause
+        })
+    }
+
+})
+/* ------------------------------------------------------- */
+
+/* ------------------------------------------------------- */
+//? use(errorHandler) kodlamanın en sonunda yer almalı.
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.errorStatusCode ?? 500
+
+	console.log("errorHandler runs");
+
+	res.status(statusCode).send({
+		error: true, // special data
+		message: err.message, // Error string Message
+		cause: err.cause, // Error optional cause
+		// stack: err.stack, // Error Details.
+	});
+};
+//? for run errorHandler call in use.
+//? It must be at last middleware.
+app.use(errorHandler);
 
 /* ------------------------------------------------------- */
 
