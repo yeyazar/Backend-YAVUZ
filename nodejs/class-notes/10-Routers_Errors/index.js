709b8d3216553ce26fd069e3a3d3a0d9ea0c8700
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-30 15:03:10 +0100

    exp10

--- nodejs/class-notes/10-Routers_Errors/index.js
+++ nodejs/class-notes/10-Routers_Errors/index.js
@@ -1,8 +1,25 @@
-"use strict";
+"use strict"
 
 const express = require("express")
 const app = express()
 
 require("dotenv").config()
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
 
+/* ------------------------------------------------------- */
+//? "Router" is special app for URL control in ExpressJS.
+
+// app.get("/", (req, res) => {
+// 	res.send({ message: "Home Page" })
+// })
+// app.get("/about", (req, res) => {
+// 	res.send({ message: "About Page" })
+// })
+// app.get("/user:userId", (req, res) => {
+// 	res.send({ message: "User Page" })
+// })
+
+const router = express.Router()
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT))
