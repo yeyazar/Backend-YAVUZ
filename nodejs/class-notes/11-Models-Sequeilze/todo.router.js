1e5a2655b07058c493eacadeec6454564f6b7e39
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-31 17:35:57 +0100

    exp11

--- nodejs/class-notes/11-Models-Sequeilze/todo.router.js
+++ nodejs/class-notes/11-Models-Sequeilze/todo.router.js
@@ -22,8 +22,9 @@ router.post("/", async (req, res) => {
 	const data = await Todo.create(req.body);
     res.send({
 		error: false,
+        body: req.body, // Send Data
         message: 'Created',
-		result: data,
+		result: data, // Receive Data
 	});
 });
 
