84f4dc97157b193977ce03af5dd19ef31570e04e
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-10-31 11:35:57 +0100

    exp11

--- nodejs/class-notes/11-Models-Sequeilze/server.js
+++ nodejs/class-notes/11-Models-Sequeilze/server.js
@@ -2,20 +2,38 @@
 /* -------------------------------------------------------
     EXPRESSJS - TODO Project with Sequelize
 ------------------------------------------------------- */
+
 const express = require("express");
 const app = express();
 
-app.all("/all", (req, res) => {
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/*-------------------------------------------------------------------------*/
+// Accept json data & convert to object:
+app.use(express.json())
+
+app.all("/", (req, res) => {
 	res.send("WELCOME TODO API");
 });
 
-require("dotenv").config();
-const PORT = process.env.PORT || 8000;
 /*-------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------*/
+
+
 
 /*-------------------------------------------------------------------------*/
-
+const errorHandler = (err, req, res, next) => {
+    const errorStatusCode = res.errorStatusCode ?? 500
+    console.log('errorHandler runned.')
+    res.status(errorStatusCode).send({
+        error: true, // special data
+        message: err.message, // error string message
+        cause: err.cause, // error option cause
+        // stack: err.stack, // error details
+        body: req.body,
+    })
+}
+app.use(errorHandler)
 /*-------------------------------------------------------------------------*/
 app.listen(PORT, () => console.log("Running -> http://127.0.0.1:" + PORT));
