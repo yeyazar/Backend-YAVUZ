12ff39018b27c0f81905c48ea49f182abbdc81e3
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-11-01 17:57:46 +0100

    up

--- nodejs/class-notes/12-TodoAPI/app.js
+++ nodejs/class-notes/12-TodoAPI/app.js
@@ -13,11 +13,12 @@ const PORT = process.env.PORT || 8000;
 // Accept json data & convert to object:
 app.use(express.json());
 
-
 // DatabaseConnection:
-app.use(require('./app/dbConnection'))
+const dbConnection = require("./app/dbConnection");
+dbConnection();
 
 // errorHandler (Catch Errors):
 app.use(require("./app/errorHandler"));
+
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
