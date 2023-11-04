660ad8d8f111ce510a0d2fdebc29c27300a8c982
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-11-04 11:54:14 +0100

    up

--- nodejs/class-notes/13-BlogAPI_Mongoose/index.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
@@ -6,23 +6,25 @@
  * $ npm init -y
  * $ npm i express dotenv express-async-errors
  * $ npm i mongoose
-*/
+ */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
-const PORT = process.env.PORT || 8000
-
-/* ------------------------------------------------------- */
-
-app.use(express.json())
-
-app.all('/', (req, res) => {
-    res.send('WELCOME TO BLOG API')
-})
-
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 
-app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+app.use(express.json());
+
+app.all("/", (req, res) => {
+	res.send("WELCOME TO BLOG API");
+});
+
+
+/* ------------------------------------------------------- */
+// error handler
+app.use(require("./src/errorHandler.js"));
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
