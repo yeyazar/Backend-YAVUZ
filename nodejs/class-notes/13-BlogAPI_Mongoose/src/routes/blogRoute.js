9e1ae9cd87527c18fe5049cffa1eeab25173e985
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-11-04 23:41:05 +0100

    up

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/blogRoute.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/blogRoute.js
@@ -1,5 +1,21 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+const router = required("express").Router();
 
+const { BlogPost } = require("../controllers/blogController.js");
+
+// ------------------------------------------
+// BlogPost
+// ------------------------------------------
+router.route("/post")
+    .get(BlogPost.list)
+    .post(BlogPost.create);
+
+router.route("/post/:postId")
+    .get(BlogPost.read)
+    .put(BlogPost.update)
+    .delete(BlogPost.delete);
+
+module.exports = router;
