762237cb8a2157efb883101d87b9ce71ab425343
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-11-04 23:37:03 +0100

    up

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
@@ -1,5 +1,40 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// https://mongoosejs.com/docs/queries.html
 
+// catch error
+require("express-async-error");
+
+const { BlogPost } = require("../models/blogModel.js");
+
+// ------------------------------------------
+// BlogPost
+// ------------------------------------------
+module.exports.BlogPost = {
+	list: async (req, res) => {
+		const data = await BlogPost.find();
+
+		res.status(200).send({
+			error: false,
+			result: data,
+		});
+	},
+
+	create: async (req, res) => {
+		const data = await BlogPost.insertOne(req.body);
+
+		res.status(201).send({
+			error: false,
+			body: req.body,
+			result: data,
+		});
+	},
+
+	read: async (req, res) => {},
+
+	update: async (req, res) => {},
+
+	delete: async (req, res) => {},
+};
