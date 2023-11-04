79233c0c3f4309882e4addc94d7df719646cd8e1
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-11-04 12:02:35 +0100

    up

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/dbConnection.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/dbConnection.js
@@ -1,5 +1,11 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 
+const mongoose = require("mongoose");
+
+mongoose
+	.connect(process.env.MONGODB || "mongodb://localhost:27017/")
+	.then(() => console.log(" * DB Connected * "))
+	.catch((err) => console.log(" * DB NOT!!! Connected * ", err));
