370f98ae11bcd10b09cdf508f3ab0d800872ef9b
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-11-05 18:56:28 +0100

    up

--- nodejs/class-notes/14-BlogAPI_UserModel/src/controllers/userController.js
+++ nodejs/class-notes/14-BlogAPI_UserModel/src/controllers/userController.js
@@ -79,12 +79,13 @@ module.exports.User = {
 
 				if (req.body?.rememberMe) {
 					// Set Cookie maxAge
-					req.sessionOptions.maxAge= 1000 * 60 * 60 * 24 * 3 // 3 days
+					req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3; // 3 days
 				}
 
 				res.status(200).send({
 					error: false,
 					result: user,
+					session: req.session,
 				});
 			} else {
 				res.errorStatusCode = 401;
